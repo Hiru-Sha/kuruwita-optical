0e87a392fdc931268eeb30d773afe996c915a623
Hiru-Sha <hirusharashmika3@gmail.com>
2026-04-20 23:03:03 +0530

    new

---
+++ frontend/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+// If you have a global CSS file, import it here, for example:
+// import './index.css'; 
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
